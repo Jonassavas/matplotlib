fe8f0d21309e7234ac376a9bcc914735f70146ad
Nyappy Chan <nothingklara@gmail.com>
2023-02-21 12:02:53 +0100

    doc: added comments to the new tests for streamplot #42

--- lib/matplotlib/tests/test_streamplot_klara.py
+++ lib/matplotlib/tests/test_streamplot_klara.py
@@ -6,6 +6,14 @@ from matplotlib.testing.decorators import image_comparison
 import matplotlib.transforms as mtransforms
 import matplotlib.lines as mlines 
     
+'''
+Untested requirement: 
+raising ValueError if shape of color and grid doesn't match
+
+Testing that the function streamplot() raises a value error if
+the 2D array color given doesn't match the shape of the grid 
+(created by the x and y input arrays).
+'''
 def test_streamplot_color_grid_shapes_not_matching():
     with pytest.raises(ValueError) as excinfo:
         plt.streamplot(np.arange(3), np.arange(3),
@@ -13,6 +21,14 @@ def test_streamplot_color_grid_shapes_not_matching():
                        color=np.random.rand(5, 5))
     assert str(excinfo.value) == "If 'color' is given, it must match the shape of the (x, y) grid"
 
+'''
+Untested requirement: 
+raising ValueError if shape of linewidth and grid doesn't match
+
+Testing that the function streamplot() raises a value error if
+the 2D array linewidth given doesn't match the shape of the grid 
+(created by the x and y input arrays).
+'''
 def test_streamplot_linewidth_grid_shape_not_matching():
     with pytest.raises(ValueError) as excinfo:
         plt.streamplot(np.arange(3), np.arange(3),
@@ -20,6 +36,15 @@ def test_streamplot_linewidth_grid_shape_not_matching():
                        color=np.random.rand(3, 3), linewidth= np.full((5, 5), np.nan))
     assert str(excinfo.value) == "If 'linewidth' is given, it must match the shape of the (x, y) grid"
 
+'''
+Untested requirement: 
+raising ValueError if shape of u (or v) and grid doesn't match
+
+Testing that the function streamplot() raises a value error if
+the 2D arrays u or v given doesn't match the shape of the grid
+(created by the x and y input arrays), here it is the u that 
+doesn't match.
+'''
 def test_streamplot_linewidth_check_u_grid_shape_not_matching():
     with pytest.raises(ValueError) as excinfo:
         plt.streamplot(np.arange(3), np.arange(3),
@@ -27,6 +52,14 @@ def test_streamplot_linewidth_check_u_grid_shape_not_matching():
                        color=np.random.rand(3, 3))
     assert str(excinfo.value) == "'u' and 'v' must match the shape of the (x, y) grid"
 
+'''
+Untested requirement: 
+raising ValueError if starting points outside of grid
+
+Testing that the function streamplot() raises a value error if
+the values in the start_points ndarray of the kind (N, 2) 
+contains points from outside the data boundaries.
+'''
 def test_streamplot_():
     with pytest.raises(ValueError) as excinfo:
         plt.streamplot(np.arange(3), np.arange(3),
